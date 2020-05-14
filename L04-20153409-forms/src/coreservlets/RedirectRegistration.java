127325d37ab0cb4b5919f6950881c396c1c2bd78
dawoori <daul@kookmin.ac.kr>
2020-05-14 17:36:32 +0900

    L06 doing

--- L04-20153409-forms/src/coreservlets/RedirectRegistration.java
+++ L04-20153409-forms/src/coreservlets/RedirectRegistration.java
@@ -24,23 +24,24 @@ public class RedirectRegistration extends HttpServlet {
 	PrintWriter out = response.getWriter();
 	String title = "Registration Form";
 	String docType = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 " + "Transitional//EN\">\n";
-	out.println(docType + "<HTML>\n" + "<HEAD><TITLE>" + title + "</TITLE></HEAD>\n" + "<BODY BGCOLOR=\"#FDF5E6\">\n"
+	out.println(
+		docType + "<HTML>\n" + "<HEAD><TITLE>" + title + "</TITLE></HEAD>\n" + "<BODY BGCOLOR=\"#FDF5E6\">\n"
 			+ "<H1 ALIGN=\"CENTER\">" + title + "</H1>\n" + "<FORM ACTION=\"registration-display\">");
 	if (request.getParameter("lastname") != null && isEmpty(request.getParameter("lastname"))) {
-	    out.println("<B>Required!!</B>");
+	    out.println("<B>Required Field!!</B>");
 	}
 	out.println("Last Name:  <INPUT TYPE=\"TEXT\" NAME=\"lastname\" VALUE=\""
 		+ Objects.toString(request.getParameter("lastname"), "") + "\"><BR>");
 	if (request.getParameter("firstname") != null && isEmpty(request.getParameter("firstname"))) {
-	    out.println("<B>Required!!</B>");
+	    out.println("<B>Required Field!!</B>");
 	}
-	out.println("First Name: <INPUT TYPE=\"TEXT\" NAME=\"firstname\"VALUE=\"" + request.getParameter("firstname")
-		+ "\"><BR>");
+	out.println("First Name: <INPUT TYPE=\"TEXT\" NAME=\"firstname\"VALUE=\""
+		+ Objects.toString(request.getParameter("firstname"), "") + "\"><BR>");
 	if (request.getParameter("emailaddress") != null && isEmpty(request.getParameter("emailaddress"))) {
-	    out.println("<B>Required!!</B>");
+	    out.println("<B>Required Field!!</B>");
 	}
 	out.println("Email address:  <INPUT TYPE=\"TEXT\" NAME=\"emailaddress\" VALUE=\""
-		+ request.getParameter("emailaddress") + "\"><BR>");
+		+ Objects.toString(request.getParameter("emailaddress"), "") + "\"><BR>");
 	out.println("<CENTER><INPUT TYPE=\"SUBMIT\"></CENTER>" + "</FORM>" + "</BODY></HTML>");
     }
 
---
+++ L06-1-20153409-req-headers/src/coreservlets/BrowserColors2.java
@@ -0,0 +1,14 @@
+package coreservlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/browser-colors")
+public class BrowserColors2 extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+	
+    }
+}
