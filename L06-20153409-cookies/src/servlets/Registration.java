cab1ec3d3b236b8d158eecb361ec3db1ad32846f
dawoori <daul@kookmin.ac.kr>
2020-06-09 17:14:49 +0900

    L07 L08 init

---
+++ L06-20153409-cookies/src/servlets/Registration.java
@@ -0,0 +1,10 @@
+package servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Registration extends HttpServlet {
+    
+}
